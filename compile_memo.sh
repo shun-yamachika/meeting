880ad3a52df9366fd97c4fe67dd28a1d66cabcd3
shun <shun@a.a>
2026-04-07 19:29:34 +0900

    update

--- compile_memo.sh
+++ compile_memo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # memo.orgからPDFを生成するスクリプト
+
+# memo.org → memo.tex エクスポート
+emacs --batch -Q \
+  --eval "(require 'ox-latex)" \
+  --eval "(add-to-list 'org-latex-classes '(\"IEEEtran\" \"\\\\documentclass[conference]{IEEEtran}\" (\"\\\\section{%s}\" . \"\\\\section*{%s}\") (\"\\\\subsection{%s}\" . \"\\\\subsection*{%s}\") (\"\\\\subsubsection{%s}\" . \"\\\\subsubsection*{%s}\")))" \
+  --visit memo.org \
+  --eval "(org-latex-export-to-latex)"
+
 platex -interaction=nonstopmode memo.tex
 pbibtex memo
 platex -interaction=nonstopmode memo.tex
